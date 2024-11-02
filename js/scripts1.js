05741543f4998fc266e5de1347300b33c6f7e6ca
RGD S★ <128194619+rgd01sh@users.noreply.github.com>
2024-11-02 22:48:29 +0300

    Add files via upload

---
+++ js/scripts1.js
@@ -0,0 +1,5 @@
+document
+  .getElementById("nextPageButton")
+  .addEventListener("click", function () {
+    window.location.href = "pages/page1.html"; // Update this to the actual next page URL
+  });
